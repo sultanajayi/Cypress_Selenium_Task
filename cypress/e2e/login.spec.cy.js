5d337b06c6c787e1f768fef057f6cbaa19d092ec
sultanajayi <hayobamidele39@gmail.com>
2024-06-20 17:56:49 +0100

    Cypress login

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,5 +1,33 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+
+describe('Login Functionality', () => {
+  beforeEach(() => {
+    cy.visit('/'); 
+  });
+
+  it('should successfully log in with valid credentials', () => {
+    cy.get('#username').type('validUsername');
+    cy.get('#password').type('validPassword');
+    cy.get('#loginButton').click();
+
+   
+    cy.url().should('not.include', '/login'); 
+  });
+
+  it('should fail to log in with invalid credentials', () => {
+    cy.get('#username').type('invalidUsername');
+    cy.get('#password').type('invalidPassword');
+    cy.get('#loginButton').click();
+
+    
+    cy.get('#errorMessage').should('be.visible');
+  });
+
+  it('should display an error message when login fails', () => {
+    cy.get('#username').type('invalidUsername');
+    cy.get('#password').type('invalidPassword');
+    cy.get('#loginButton').click();
+
+    
+    cy.get('#errorMessage').should('contain', 'Invalid credentials');
+  });
+});
