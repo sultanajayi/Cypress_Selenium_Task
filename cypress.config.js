6e8bb648521eaa7b4dd5faf3ce6afde99e485f96
sultanajayi <hayobamidele39@gmail.com>
2024-06-20 16:59:47 +0100

    Cypress and configuration setup

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: '',
+    defaultCommandTimeout: 10000,
+    experimentalWebKitSupport: true,
+    experimentalRunAllSpecs: true,
+    includeShadowDom: true,
+    chromeWebSecuritySupport: true,
+    viewportHeight: 800,
+    viewportWidth: 1500,
+    watchForFileChanges: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
